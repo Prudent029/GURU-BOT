b08de885769ef31d437e4f10db8e183784e3c165
Prudent029 <160515915+Prudent029@users.noreply.github.com>
2024-02-21 09:49:48 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['8069908432', 'Prudent029, true],
+  ['+2348069908432', 'Prudent029, true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
